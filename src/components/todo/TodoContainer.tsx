47895eecc0b26bbe4d7c3a78c734b5932283560b
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-07-29 12:19:40 +0600

    first commit

---
+++ src/components/todo/TodoContainer.tsx
@@ -0,0 +1,24 @@
+import AddTodoModal from "./AddTodoModal";
+import TodoCard from "./TodoCard";
+
+const TodoContainer = () => {
+  return (
+    <div>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
+        <button>Filter</button>
+      </div>
+
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoContainer;
